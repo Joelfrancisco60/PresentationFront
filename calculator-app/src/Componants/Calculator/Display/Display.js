0eb5b005e85740c39722eac9a7d7571b1b6dab2b
yassine <42623533+yassine315@users.noreply.github.com>
2025-01-17 16:51:42 +0100

    use mixin

--- calculator-app/src/Componants/Calculator/Display/Display.js
+++ calculator-app/src/Componants/Calculator/Display/Display.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Display = ({ value }) => {
-  return <div className="display">
-      <span className="display__result">{value}</span>
-    </div>;
+  if (value === "Error")
+    return <div className="display">
+        <span className="display__result display__result--error">{value}</span>
+      </div>;
+  else
+    return <div className="display">
+        <span className="display__result display__result--default">{value}</span>
+      </div>;
 };
 
 export default Display;
