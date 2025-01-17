15db7be5e03a303fcd44312201e99d197135183e
yassine <42623533+yassine315@users.noreply.github.com>
2025-01-17 14:17:00 +0100

    add theming

--- calculator-app/src/Componants/Calculator/Display/Display.js
+++ calculator-app/src/Componants/Calculator/Display/Display.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Display = ({ value }) => {
-  return <div className="display">{value}</div>;
+  return <div className="display">
+    <span className="display__result">{value}</span>
+    </div>;
 };
 
 export default Display;
